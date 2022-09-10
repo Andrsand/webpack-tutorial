1b74631868fc1d57e90e906fefb6d879a59779ea
andrsand <angorko@list.ru>
2022-09-10 21:37:38 +0300

    Create a new project
    * Create a webpack-tutorial project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//console.log('Как интересно!')
+// создаем элемент заголовка
+const heading = document.createElement('h1')
+heading.textContent = 'Как интересно!'
+
+// добавляем заголовок в DOM
+const root = document.querySelector('#root')
+root.append(heading)
