bd476e2ddc659ce43040126f24619d2ccf640214
andrsand <angorko@list.ru>
2022-09-10 22:06:52 +0300

    feature: Creating a class
    * Creating a class property without a constructor in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 //console.log('Как интересно!')
 // создаем элемент заголовка
+// const heading = document.createElement('h1')
+// heading.textContent = 'Как интересно!'
+
+// добавляем заголовок в DOM
+// const root = document.querySelector('#root')
+// root.append(heading)
+
+// создание свойства класса без конструктора
+class Game {
+    name = 'Violin Charades'
+}
+const myGame = new Game()
+
+// создаем параграф
+const p = document.createElement('p')
+p.textContent = `I like ${myGame.game}.`
+
+// создаем элемент заголовка
 const heading = document.createElement('h1')
 heading.textContent = 'Как интересно!'
 
-// добавляем заголовок в DOM
+// добавляем параграф и заголовок в DOM
 const root = document.querySelector('#root')
-root.append(heading)
+root.append(heading, p)
